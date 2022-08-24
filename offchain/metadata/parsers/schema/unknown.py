e0a367580fdfe2b8eac47624fda680c6ecdc7170
Steve Jiang <steve@ourzora.com>
2022-08-24 18:34:35 +0200

    add more docstrings

--- offchain/metadata/parsers/schema/unknown.py
+++ offchain/metadata/parsers/schema/unknown.py
@@ -121,6 +121,15 @@ class UnknownParser(SchemaParser):
         return details
 
     def parse_metadata(self, token: Token, raw_data: dict, *args, **kwargs) -> Optional[Metadata]:
+        """Given a token and raw data returned from the token uri, return a normalized Metadata object.
+
+        Args:
+            token (Token): token to process metadata for.
+            raw_data (dict): raw data returned from token uri.
+
+        Returns:
+            Optional[Metadata]: normalized metadata object, if successfully parsed.
+        """
         mime, _ = self.fetcher.fetch_mime_type_and_size(token.uri)
 
         return Metadata(
@@ -137,4 +146,13 @@ class UnknownParser(SchemaParser):
         )
 
     def should_parse_token(self, token: Token, raw_data: dict, *args, **kwargs) -> bool:
+        """Return whether or not a collection parser should parse a given token.
+
+        Args:
+            token (Token): the token whose metadata needs to be parsed.
+            raw_data (dict): raw data returned from token uri.
+
+        Returns:
+            bool: whether or not the collection parser handles this token.
+        """
         return True
