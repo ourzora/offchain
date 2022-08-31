705b69ff2065745310368f89da83d6e6acd8de09
Matt Malec <matt@mattmalec.com>
2022-08-30 20:26:36 -0400

    Update init for parser

--- offchain/metadata/parsers/__init__.py
+++ offchain/metadata/parsers/__init__.py
@@ -1,14 +1,11 @@
-from offchain.base.types import StringEnum
-
 from .base_parser import BaseParser
+from .collection.autoglyphs import AutoglyphsParser
 from .collection.collection_parser import CollectionParser
-from .collection.nouns_parser import NounsParser
+from .collection.ens import ENSParser
+from .collection.foundation import FoundationParser
+from .collection.punks import PunksParser
+from .collection.superrare import SuperRareParser
+from .collection.nouns import NounsParser
 from .schema.opensea import OpenseaParser
-from .schema.unknown import UnknownParser
 from .schema.schema_parser import SchemaParser
-
-
-class ParserType(StringEnum):
-    OpenseaParser = OpenseaParser.__name__
-    NounsParser = NounsParser.__name__
-    UnknownParser = UnknownParser.__name__
+from .schema.unknown import UnknownParser
