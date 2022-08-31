4db88ed4fc15c12022bd50d86992e00d045c7c8a
Matt Malec <matt@mattmalec.com>
2022-08-30 20:27:53 -0400

    Add to default collection parsers

--- offchain/metadata/pipelines/metadata_pipeline.py
+++ offchain/metadata/pipelines/metadata_pipeline.py
@@ -21,6 +21,7 @@ from offchain.metadata.parsers import (
     OpenseaParser,
     SuperRareParser,
     AutoglyphsParser,
+    NounsParser,
 )
 from offchain.metadata.parsers.collection.punks import PunksParser
 from offchain.metadata.parsers.schema.unknown import UnknownParser
@@ -54,7 +55,7 @@ DEFAULT_ADAPTER_CONFIGS: list[AdapterConfig] = [
     ),
 ]
 
-COLLECTION_PARSERS = [ENSParser, FoundationParser, SuperRareParser, PunksParser, AutoglyphsParser]
+COLLECTION_PARSERS = [ENSParser, FoundationParser, SuperRareParser, PunksParser, NounsParser, AutoglyphsParser]
 SCHEMA_PARSERS = [OpenseaParser, UnknownParser]
 
 
