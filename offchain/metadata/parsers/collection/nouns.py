7db08000636329b354bac6c1d4534472ba0a1d66
Matt Malec <matt@mattmalec.com>
2022-09-01 11:47:07 -0400

    Clean up Nouns parser

--- offchain/metadata/parsers/collection/nouns.py
+++ offchain/metadata/parsers/collection/nouns.py
@@ -20,7 +20,6 @@ class NounsParser(CollectionParser):
         raw_image_uri = raw_data.get("image")
         image_uri = quote(self.fetcher.fetch_content(raw_image_uri))
 
-        # image_uri = quote(self.decode_base64(raw_image_uri))
         return MediaDetails(uri=image_uri, size=None, sha256=None, mime_type="image/svg+xml")
 
     def seeds(self, token: Token) -> Optional[Seeds]:
@@ -78,14 +77,16 @@ class NounsParser(CollectionParser):
 
     def parse_metadata(self, token: Token, raw_data: dict, *args, **kwargs) -> Metadata:
         token.uri = self.get_uri(token)
+
         raw_data = self.fetcher.fetch_content(token.uri)
+        mime_type, _ = self.fetcher.fetch_mime_type_and_size(token.uri)
 
         return Metadata(
             token=token,
             raw_data=raw_data,
             name=raw_data.get("name"),
             description=raw_data.get("description"),
-            mime_type="application/json",
+            mime_type=mime_type,
             image=self.get_image(raw_data),
             attributes=self.get_seed_attributes(token),
         )
