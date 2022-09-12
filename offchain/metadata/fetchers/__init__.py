9cb31e21d67280919e882b3e97eca4927e0a84f1
Steve Jiang <steve@ourzora.com>
2022-09-12 17:56:12 +0200

    default chain identifier and scope hoist for easier imports

--- offchain/metadata/fetchers/__init__.py
+++ offchain/metadata/fetchers/__init__.py
@@ -1,8 +1,2 @@
-from offchain.base.types import StringEnum
-
 from .base_fetcher import BaseFetcher
 from .metadata_fetcher import MetadataFetcher
-
-
-class FetcherType(StringEnum):
-    MetadataFetcher = MetadataFetcher.__name__
--- offchain/metadata/models/metadata_processing_error.py
+++ offchain/metadata/models/metadata_processing_error.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from offchain.base.base_model import BaseModel
 from offchain.metadata.models.token import Token
 
@@ -8,19 +6,12 @@ class MetadataProcessingError(BaseModel):
     """Interface for metadata processing errors and relevant contextual information.
 
     Attributes:
-        chain_identifier (str): identifier for network and chain of token,
-            formatted as NETWORK_NAME-CHAIN_NAME (e.g. "ETHEREUM-MAINNET").
-        collection_address (str): collection address of the token.
-        token_id (int): token id of the token.
-        uri (str): metadata uri of the token.
+        token (Token): a Token interface with all information required to uniquely identify an NFT
         error_type (str): the class of caught exception.
         error_message (str): the error message of the caught exception.
     """
 
-    chain_identifier: str
-    collection_address: str
-    token_id: int
-    uri: Optional[str]
+    token = Token
 
     error_type: str
     error_message: str
@@ -28,10 +19,7 @@ class MetadataProcessingError(BaseModel):
     @staticmethod
     def from_token_and_error(token: Token, e: Exception) -> "MetadataProcessingError":
         return MetadataProcessingError(
-            chain_identifier=token.chain_identifier,
-            collection_address=token.collection_address,
-            token_id=token.token_id,
-            uri=token.uri,
+            token=token,
             error_type=e.__class__.__name__,
             error_message=str(e),
         )
