1bf141c005b5acfca40353bcdf81369b11bc4d23
Steve Jiang <steve@ourzora.com>
2022-09-15 19:52:07 +0200

    hoist imports into package root level

--- offchain/__init__.py
+++ offchain/__init__.py
@@ -1,3 +1,7 @@
-from offchain.metadata.models.token import Token  # noqa
-from offchain.metadata.pipelines.metadata_pipeline import \
-    MetadataPipeline  # noqa
+from offchain.metadata import (
+    Metadata,
+    MetadataFetcher,
+    MetadataPipeline,
+    MetadataProcessingError,
+    Token,
+)  # noqa
--- offchain/metadata/adapters/__init__.py
+++ offchain/metadata/adapters/__init__.py
@@ -1,5 +1,5 @@
 from .arweave import ARWeaveAdapter
-from .base_adapter import BaseAdapter
+from .base_adapter import AdapterConfig, BaseAdapter
 from .data_uri import DataURIAdapter
 from .http_adapter import HTTPAdapter
 from .ipfs import IPFSAdapter
